f95856989ca06820fea9ca54e3cdef977a490094
plb97 <p.lhb97@gmail.com>
2017-12-19 13:28:44 +0100

    Initial Commit

---
+++ Cass/Cass.h
@@ -0,0 +1,13 @@
+//
+//  Cass.h
+//  Cass
+//
+//  Created by Philippe on 19/12/2017.
+//  Copyright © 2017 PLHB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cass : NSObject
+
+@end
